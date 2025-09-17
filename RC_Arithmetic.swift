0d6c455d62de74a3fa49d5390906c72a5b3c31ea
Cranjis McBasketball <rolando.caballero100@student.tsc.edu>
2025-09-17 08:22:13 -0500

    Add files via upload

---
+++ RC_Arithmetic.swift
@@ -0,0 +1,34 @@
+// + Addition
+// - Subtraction
+// * Multiplication
+// Division
+
+//Operations with numbers
+// + operator for Addition
+let sum = 23 + 40
+
+// - operator for Subtraction
+let result = 32 - sum
+
+// * operator for Division
+let total = result * 5
+
+// / operator for Division
+let divide = total/10
+
+print(sum)
+print(result)
+print(total)
+print(divide) 
+
+// Operators can only work with the operands of the same type
+let a = 12
+let b = 12.0
+// let c = a + b
+
+ let c = Double(a) + b
+ 
+ // Using compound assignment Operators
+ 
+ var aa = 1
+ //aa is now equal to 3
