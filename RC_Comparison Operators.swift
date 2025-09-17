b71970f6a8d27893b4c901841ddd45ab4f44de4d
Cranjis McBasketball <rolando.caballero100@student.tsc.edu>
2025-09-17 14:59:58 -0500

    Add files via upload

---
+++ RC_Comparison Operators.swift
@@ -0,0 +1,22 @@
+#! usr/bin/env swift
+
+//Using Comparison Operators
+// ==  Equal to
+// !=  Not Equal to
+// >   Greater than
+// <   Less Than
+// >=  Greater Than or Equal To
+// <=  Less Than or Equal to
+
+// Comparison Operators
+print(1 == 1) // Equal to, true because 1 is = to 1
+
+print(2 != 1) // Not Equal to, true because 2 is not = 1
+
+print(2 > 1)  // Greater than, true because 2 is > 1
+
+print(1 < 2)  //Less than, true because 1 is < 2
+
+print(1 >= 1) //Greater than or equal to, true because 1 is >= 1
+
+print(2 <= 1) //Less than or equal to, false bceause 2 is not <= 1
