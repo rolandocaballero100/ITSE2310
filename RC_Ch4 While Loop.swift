c7fb5a8f19726ed02b359022864a9f761af6baff
Cranjis McBasketball <rolando.caballero100@student.tsc.edu>
2025-10-07 16:52:24 -0500

    Add files via upload

---
+++ RC_Ch4 While Loop.swift
@@ -0,0 +1,14 @@
+#! usr/bin/env
+
+// while loop
+
+while condition == true {
+	code
+}
+
+// condition is checked, and if true, the set of statements are executed
+var y = 100
+while y < 50 {
+	y += 5 
+	print("y is \(y)")
+}
