86d4342ec66ad514cdd7c47871c7b1272f252a15
Cranjis McBasketball <rolando.caballero100@student.tsc.edu>
2025-10-07 16:49:11 -0500

    Add files via upload

---
+++ RC_Ch4 Repeat While.swift
@@ -0,0 +1,13 @@
+#! usr/bin/env swift
+
+//repeat{
+//	code
+//} while condition == true
+
+// repeat loop
+// set of statements is executed, then the condition is checked
+var x = 0
+repeat{
+	x += 5
+	print("x is \(x)")
+} while x < 50
